e8021186dba115aaec2a700c947d230c2fdd41eb
Ana Paula <paula.torres@unas.edu.pe>
2026-04-27 11:28:29 -0500

    Actualizo calculadora.cc con nueva versión

--- calculadora.cc
+++ calculadora.cc
@@ -1,17 +1,24 @@
-#include <iostream> 
+#include <iostream>
 using namespace std;
-int main (){
-int a,b;
-cout << "ingrese su numero " ;
-cin>> a >> b;
 
-cout <<"suma " << a+b << endl;
-cout << "resta " << a-b << endl ;
-cout << "multiplicacion "<< a*b << endl;
+int main() {
+    int a, b;
 
-if (b==0)
-  cout <<"no se divide entre 0 gracias ";
-else 
-  cout <<"divicion "<< a/b << endl;
-return 0; 
+    cout << "Ingrese el primer numero: ";
+    cin >> a;
+
+    cout << "Ingrese el segundo numero: ";
+    cin >> b;
+
+    cout << "Suma: " << a + b << endl;
+    cout << "Resta: " << a - b << endl;
+    cout << "Multiplicacion: " << a * b << endl;
+
+    if (b != 0) {
+        cout << "Division: " << (float)a / b << endl;
+    } else {
+        cout << "No se puede dividir entre 0" << endl;
+    }
+
+    return 0;
 }
